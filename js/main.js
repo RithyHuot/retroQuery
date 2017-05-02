3ebbe1b2fd38d35d7e5915d67f2330f51004c9a8
Rithy Huot <RithyBHuot@gmail.com>
2017-05-02 19:36:13 -0400

    Complete demo example with rQuery

--- js/main.js
+++ js/main.js
@@ -1,17 +1,28 @@
-$r('.addListItem').on('click', () => {
-  let list = $r('ol');
-  let input = $r('#listItem').elements[0].value;
-  let counter = input.length;
-  if ( input.length > 0 ) {
-    list.append(`<li class='list-${counter}'>`+ input +'</li>');
-    $r('#listItem').elements[0].value = '';
-  }
+document.addEventListener('DOMContentLoaded', () => {
+  const button = $r('button');
+  $r('.addListItem').on('click', () => {
+    let list = $r('ol');
+    let input = $r('#listItem').elements[0].value;
+    if ( input.length > 0 ) {
+      list.append(
+        `<li>`+
+        input +
+        "<button class='removeButton'>X</button>" +
+        '</li>');
 
-  $r('li').on('click', () => {
-    $r('li').toggleClass('strike');
-  });
+        $r('#listItem').elements[0].value = '';
 
-  $r('.removeListItem').on('click', () => {
-    $r('li').remove();
+        $r(".removeButton").on('click', (e) => {
+          $r(e.currentTarget).parent().remove();
+        });
+
+        $r('li').on('click', (e) => {
+          $r(e.currentTarget).toggleClass('strike');
+        });
+    }
+
+    $r('.removeListItem').on('click', () => {
+      $r('li').remove();
+    });
   });
 });
